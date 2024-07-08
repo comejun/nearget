2dc8cac1b767ae04f56c6248580a634d207f68b6
comejun <comejun@naver.com>
2024-07-08 14:53:55 +0900

    fixed scss

--- back/src/main/java/com/nearget/back/component/RestaurantDBSchedular.java
+++ back/src/main/java/com/nearget/back/component/RestaurantDBSchedular.java
@@ -11,7 +11,8 @@ public class RestaurantDBSchedular {
 
     private final RestaurantService restaurantService;
 
-    @Scheduled(fixedDelay = 100000)
+    // 매일 0시 0분 0초에 실행
+    @Scheduled(cron = "0 0 0 * * *")
     public void scheduleRestaurant() {
 
         // 오류 발생 전까지 saveAllRestaurant page 0부터 1씩 증가하며 실행
