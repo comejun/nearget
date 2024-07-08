900e5e9522adcb59879a16adaf290a5042f37319
hoseonYoo <hohosun2i@gmail.com>
2024-07-08 15:36:15 +0900

    아이콘업데이트

--- front/src/layouts/MainTodayGet.js
+++ front/src/layouts/MainTodayGet.js
@@ -10,7 +10,16 @@ export default function MainTodayGet() {
             <div className="scrollContent">
               <img src={process.env.PUBLIC_URL + "/assets/imgs/sample.png"} />
             </div>
-            <div className="scrollTextContent"></div>
+            <div className="scrollTextContent">
+              <div className="scrollTextLi">
+                <p>02-777-2579</p>
+                <span>연락하기</span>
+              </div>
+              <div className="scrollTextLi">
+                <p>02-777-2579</p>
+                <span>주소복사</span>
+              </div>
+            </div>
           </li>
           <li>zz</li>
           <li>zz</li>
