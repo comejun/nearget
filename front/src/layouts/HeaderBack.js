e96c6493ed41760df92cb0bad7972ade07b362a9
hoseonYoo <hohosun2i@gmail.com>
2024-07-08 22:42:08 +0900

    페이지작업용UseCustomMove연결 업데이트

--- front/src/layouts/HeaderBack.js
+++ front/src/layouts/HeaderBack.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import UseCustomMove from "../hooks/useCustomMove";
 
 export default function HeaderBack({ imgSrc }) {
+  const { moveToMain } = UseCustomMove();
+
   return (
     <header>
       <div className="headerWrap">
         {/* 메인페이지 헤더 */}
-        <div className="headerBackContent">
-          <Link to="/list">
-            <img src={process.env.PUBLIC_URL + "/assets/imgs/icon/ic_back.png"} alt="searchIcon" height="36px" />
-          </Link>
+        <div className="headerBackContent" onClick={moveToMain}>
+          <img src={process.env.PUBLIC_URL + "/assets/imgs/icon/ic_back.png"} alt="searchIcon" height="36px" />
         </div>
         <img className="headerh1Con" src={process.env.PUBLIC_URL + imgSrc} alt="searchIcon" />
       </div>
