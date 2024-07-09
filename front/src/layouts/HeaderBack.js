a98dc245deaee9e638463573d47f6a791fb2dab7
hoseonYoo <hohosun2i@gmail.com>
2024-07-09 12:02:44 +0900

    클러스터이미지파일, 뒤로가기 수정 업로드

--- front/src/layouts/HeaderBack.js
+++ front/src/layouts/HeaderBack.js
@@ -1,15 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import UseCustomMove from "../hooks/useCustomMove";
 
 export default function HeaderBack({ imgSrc }) {
+  const navigate = useNavigate();
   const { moveToMain } = UseCustomMove();
+
+  const goBackOrToMain = () => {
+    if (navigate.length > 2) {
+      navigate(-1);
+    } else {
+      moveToMain();
+    }
+  };
 
   return (
     <header>
       <div className="headerWrap">
         {/* 메인페이지 헤더 */}
-        <div className="headerBackContent" onClick={moveToMain}>
+        <div className="headerBackContent" onClick={goBackOrToMain}>
           <img src={process.env.PUBLIC_URL + "/assets/imgs/icon/ic_back.png"} alt="searchIcon" height="36px" />
         </div>
         <img className="headerh1Con" src={process.env.PUBLIC_URL + imgSrc} alt="searchIcon" />
