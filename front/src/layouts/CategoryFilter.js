3153ea2a7de75b19874af048a81a53dc9163dd0f
comejun <comejun@naver.com>
2024-07-10 11:46:32 +0900

    카테고리 데이터 불러오기, redux 스토어에 등록 추가

--- front/src/layouts/CategoryFilter.js
+++ front/src/layouts/CategoryFilter.js
@@ -1,45 +1,37 @@
 import React from "react";
-// import useCategories from "../hooks/useCategories";
-// import { API_SERVER_HOST } from "../api/memberAPI";
-// import { useDispatch, useSelector } from "react-redux";
-// import { setCategory } from "../slices/categorySlice";
+import useCategories from "../hooks/useCategories";
+import { API_SERVER_HOST } from "../staticData";
+import { useDispatch, useSelector } from "react-redux";
+import { setCategory } from "../slices/categorySlice";
 
-// const host = API_SERVER_HOST;
+const host = API_SERVER_HOST;
 
 export default function CategoryFilter() {
-  // const categories = useCategories(host);
-  // const dispatch = useDispatch();
+  const categories = useCategories(host);
+  const dispatch = useDispatch();
 
   // // 셀렉터로 카테고리 가져오기
-  // const categoryFilter = useSelector((state) => state.categorySlice);
-  // const handleCategoryFilter = (e) => {
-  //   // setCategoty 로 카테고리 변경하기
-  //   dispatch(setCategory(e.target.dataset.value));
-  //   console.log(categoryFilter);
-  // };
+  const categoryFilter = useSelector((state) => state.categorySlice);
+  const handleCategoryFilter = (e) => {
+    // setCategoty 로 카테고리 변경하기
+    dispatch(setCategory(e.target.dataset.value));
+  };
 
   return (
     <div className="filterWrap">
       <div className="filterContainer">
         <ul>
-          <li className="activeFilter">전체</li>
-          <li>한식</li>
-          <li>중식</li>
-          <li>양식</li>
-          <li>일식</li>
-          <li>카페</li>
-          <li>술집</li>
-          {/* <li onClick={handleCategoryFilter} data-value={"ALL"} className={categoryFilter.category === "ALL" ? "activeFilter" : ""}>
+           <li onClick={handleCategoryFilter} data-value={"ALL"} className={categoryFilter.category === "ALL" ? "activeFilter" : ""}>
             전체
-          </li> */}
-          {/* {Object.entries(categories).length > 0 &&
+          </li>
+           {Object.entries(categories).length > 0 &&
             Object.entries(categories).map(([key, value], index) => (
               <React.Fragment key={index}>
                 <li onClick={handleCategoryFilter} data-value={key} className={categoryFilter.category === key ? "activeFilter" : ""}>
                   {value}
                 </li>
               </React.Fragment>
-            ))} */}
+            ))}
         </ul>
       </div>
     </div>
