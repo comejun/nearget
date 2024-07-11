a1e576b69e51e9eb0901b5b21875751a324bd5c7
comejun <comejun@naver.com>
2024-07-11 12:22:01 +0900

    클러스터 중첩 문제 해결

--- front/src/components/map/KakaoMap.js
+++ front/src/components/map/KakaoMap.js
@@ -118,14 +118,15 @@ const KakaoMap = () => {
         if(map){
             console.log("클러스터 생성");
 
-            cluster.clear();
 
             // clusterMarkers함수에 changePopup함수를 인자로 넘겨주어 마커 클릭시 팝업창을 띄울 수 있도록 함
-            const markers = await clustererMarkers(mapData);
+            await clustererMarkers(mapData).then((res) => {
+                cluster.clear();
+                cluster.addMarkers(res);
+            });
 
             console.log("클러스터에 마커 추가");
             // 클러스터에 마커 추가
-            cluster.addMarkers(markers);
 
         }
     }
