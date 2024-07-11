a5d6cd34bf3b875bb33206563627490e3d4910f8
comejun <comejun@naver.com>
2024-07-11 17:12:06 +0900

    식당 좌표 수정

--- back/src/main/java/com/nearget/back/service/RestaurantServiceImpl.java
+++ back/src/main/java/com/nearget/back/service/RestaurantServiceImpl.java
@@ -86,7 +86,7 @@ public class RestaurantServiceImpl implements RestaurantService {
     private ProjCoordinate getProjCoordinate(BusinessInfo businessInfo) {
         CRSFactory crsFactory = new CRSFactory();
         CoordinateReferenceSystem wgs84System = crsFactory.createFromParameters("WGS84", "+proj=longlat +datum=WGS84 +no_defs");
-        CoordinateReferenceSystem epsgSystem = crsFactory.createFromParameters("EPSG:2097", "+proj=tmerc +lat_0=38 +lon_0=127 +k=1 +x_0=200000 +y_0=500000 +ellps=bessel +units=m +no_defs +towgs84=-115.80,474.99,674.11,1.16,-2.31,-1.63,6.43");
+        CoordinateReferenceSystem epsgSystem = crsFactory.createFromParameters("EPSG:5174", "+proj=tmerc +lat_0=38 +lon_0=127.0028902777778 +k=1 +x_0=200000 +y_0=500000 +ellps=bessel +units=m +no_defs +towgs84=-115.80,474.99,674.11,1.16,-2.31,-1.63,6.43");
         ProjCoordinate p = new ProjCoordinate(parseDoubleOrDefault(businessInfo.X, 0), parseDoubleOrDefault(businessInfo.Y, 0));
         ProjCoordinate p2 = new ProjCoordinate();
         new CoordinateTransformFactory().createTransform(epsgSystem, wgs84System).transform(p, p2);
