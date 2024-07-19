872936161899f66797d51fb5dcc45b6d2575f554
comejun <comejun@naver.com>
2024-07-19 15:44:14 +0900

    배포용 수정

--- back/src/main/java/com/nearget/back/component/DBSchedular.java
+++ back/src/main/java/com/nearget/back/component/DBSchedular.java
@@ -45,19 +45,19 @@ public class DBSchedular {
 
     // 매일 0시 0분 0초에 실행
     //    @Scheduled(cron = "0 0 0 * * *")
-   /* @Scheduled(fixedDelay = 1000 * 60 * 60)
+    @Scheduled(fixedDelay = 1000 * 60 * 60)
     public void scheduleRestaurant() {
     // 오류 발생 전까지 saveAllRestaurant page 0부터 1씩 증가하며 실행
-    for (long i = 0; i < 51; i++) {
+    /*for (long i = 0; i < 51; i++) {
         restaurantService.saveAllRestaurant(i);
     }
-
+*/
     // 2분 뒤에 실행
-    try {
+    /*try {
         Thread.sleep(1000 * 60 * 10);
     } catch (InterruptedException e) {
         e.printStackTrace();
-    }
+    }*/
         saveRestaurantsDataOptimized();
         scheduleDistrict();
     }
@@ -68,7 +68,7 @@ public class DBSchedular {
         for (int i = 0; i < SmallDistrictEnum.values().length; i++) {
             smallDistrictService.saveSmallDistrict(SmallDistrictEnum.values()[i]);
         }
-    }*/
+    }
 
 
     public void saveRestaurantsDataOptimized() {
